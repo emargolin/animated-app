e1344c5dac945fb997d4194f8235c2d47c108860
Elena Margolin <emargolin@apple.com>
2025-04-16 10:32:01 -0700

    Initial commit

---
+++ AnimatedApp/AnimatedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedAppApp.swift
+//  AnimatedApp
+//
+//  Created by Elena Margolin on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedApp/OnboardingView.swift
@@ -0,0 +1,44 @@
+//
+//  OnboardingView.swift
+//  AnimatedApp
+//
+//  Created by Elena Margolin on 4/14/25.
+//
+
+import SwiftUI
+import RiveRuntime
+
+struct OnboardingView: View {
+    let button = RiveViewModel(fileName: "graybutton-2")
+    
+    var body: some View {
+        ZStack {
+            RiveViewModel(fileName: "background_bubbles").view()
+                .ignoresSafeArea()
+                .blur(radius: 10)
+                .background(
+                    Image("y2k")
+                        .ignoresSafeArea()
+                        .blur(radius: 2)
+                )
+            
+            button.view()
+                .frame(width: 236, height: 64)
+                .overlay(
+                    Label("Enter", systemImage: "arrow.forward")
+                        .offset(x: 4, y: 1)
+                        .font(.title2)
+                        .bold()
+                )
+                .onTapGesture {
+                    button.play(animationName: "active2")
+                }
+        }
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
